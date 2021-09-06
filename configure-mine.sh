8c32bb18d8cfca66a9d3254af73df7c526fc91cd
Darkalo117 <54092146+Darkalo117@users.noreply.github.com>
2021-09-05 20:06:25 -0500

    Update configure-mine.sh

--- configure-mine.sh
+++ configure-mine.sh
@@ -3,9 +3,5 @@
 # Update apt cache.
 sudo apt-get update
 
-# Install Nginx.
-sudo apt install openjdk-11-jre openjdk-11-jdk
-wget https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar -O server.jar
-toque eula.txt
-
-echo 'eula = true'> eula.txt
+# Set the home page.
+echo "<html><body><h2>Bienvenido a mi pagina web creada con la consola de Azure mi nobre es: $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
